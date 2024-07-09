45db7e660ba39b05f30a62566f0e7af5576083be
Maldonado <joseromaldo.1997@gmail.com>
2024-07-09 13:42:58 -0600

    Creación de métodos para el tipo de dato string

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Tarea 1 INGENERÍA DE SOFTWARE 2")
+
+console.log('Ejemplo para datos tipo STRING')
+let cadena = 'Este es un ejemplo para el tipo de dato string'
+
+// 1. Convertir toda la cadena a mayúsculas usando toUpperCase()
+let mayusculas = cadena.toUpperCase();
+console.log(mayusculas); // "ESTE ES UN EJEMPLO PARA EL TIPO DE DATO STRING"
+
+// 2. Reemplazar la palabra "string" por "cadena" usando replace()
+let reemplazada = cadena.replace('string', 'cadena');
+console.log(reemplazada); // "Este es un ejemplo para el tipo de dato cadena"
+
+// 3. Dividir la cadena en un array de palabras usando split()
+let palabras = cadena.split(' ');
+console.log(palabras); // ["Este", "es", "un", "ejemplo", "para", "el", "tipo", "de", "dato", "string"]
+
+
+
+
+
+console.log('Ejemplo para datos tipo NUMBER')
+console.log('Ejemplo para datos tipo BOOL')
+console.log('Ejemplo para datos tipo OBJECT')
+console.log('Ejemplo para datos tipo ARRAY')
